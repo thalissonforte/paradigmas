e139f38d9018dc7a9dd9d089c40597b9d6fec937
Thalisson Forte <thalisson1forte@gmail.com>
2019-04-24 10:40:27 -0300

    Update pratica2.pl

--- praticas/logico/pratica2.pl
+++ praticas/logico/pratica2.pl
@@ -16,3 +16,14 @@ positivos1([H|T],L) :- H =< 0, positivos1(T,L).
 positivos2([],[]).
 positivos2([H|T],L) :- H > 0, L = [H|Resto], positivos2(T,Resto).
 positivos2([H|T],L) :- H =< 0, positivos2(T,L).
+
+%7
+largest1([X],X).
+largest1([X|Xs],X) :- largest1(Xs,Y), X>=Y.
+largest1([X|Xs],N) :- largest1(Xs,N), N>X.
+
+largest2([X|Xs], N) :- aux(Xs, X, N).
+aux([], N, N).
+aux([X|Xs], M, N) :-
+   M1 is max(X, M),
+   aux(Xs, M1, N).
